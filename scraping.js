bc86a17d7748eda00441a6c1aaba965d96be360f
Pranay1237 <mudhirajpranay30@gmail.com>
2024-04-19 10:19:52 +0530

    changed the scraping functions to other folder

---
+++ scraping.js
@@ -0,0 +1,110 @@
+import puppeteer from 'puppeteer';
+import { JSDOM } from 'jsdom';
+import axios from 'axios';
+
+
+const scrapeLeetcode = async () => {
+	try {
+		const response = await axios.get('https://leetcode.com/graphql?query={ allContests { title titleSlug startTime duration __typename } }');
+		return response.data;
+	} catch (error) {
+		throw new Error('An error occurred while scraping the contests.');
+	}
+};
+
+const scrapeCodechef = async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.setViewport({ width: 1080, height: 1024 });
+
+	await page.goto('https://www.codechef.com/contests', { waitUntil: 'networkidle0' });
+
+	const htmlContent = await page.content();
+
+	await page.close();
+
+	const dom = new JSDOM(htmlContent);
+	const tbody = dom.window.document.querySelectorAll("tbody");
+	const table = tbody[1];
+	const rows = table.querySelectorAll("tr");
+
+	const contests = [];
+
+	for (let i = 0; i < rows.length; i++) {
+		const cols = rows[i].querySelectorAll("td");
+		const name = cols[0].textContent.trim();
+		const start = cols[1].textContent.trim();
+		const end = cols[2].textContent.trim();
+		const duration = cols[3].textContent.trim();
+		const link = cols[1].querySelector("a").getAttribute("href");
+
+		const contest = {
+			name: name,
+			start: start,
+			end: end,
+			duration: duration,
+			link: link
+		};
+
+		contests.push(contest);
+
+	}
+
+	const contestsJSON = JSON.stringify(contests);
+	return contestsJSON;
+};
+
+const scrapeCodeforces = async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.setViewport({ width: 1080, height: 1024 });
+
+	await page.goto('https://codeforces.com/contests', { waitUntil: 'networkidle0' });
+
+	const htmlContent = await page.content();
+
+	await page.close();
+
+	const contests = [];
+
+	const dom = new JSDOM(htmlContent);
+	const table = dom.window.document.querySelectorAll("table");
+	const rows = table[0].querySelectorAll("tr");
+	for(let i = 1; i < rows.length; i++) {
+		const cols = rows[i].querySelectorAll("td");
+		const name = cols[0].textContent.trim();
+		const start = cols[2].textContent.trim();
+		const duration = cols[3].textContent.trim();
+		const startsIn = cols[4].textContent.trim();
+		let register = cols[5].querySelector("a");
+		if(register)
+			register = register.getAttribute("href");
+		else
+			register = "";
+
+		const contest = {
+			name: name,
+			start: start,
+			duration: duration,
+			startsIn: startsIn,
+			register: (register != "" ? "https://codeforces.com" + register : "")
+		};
+
+		contests.push(contest);
+		
+	}
+	return JSON.stringify(contests);
+};
+
+const scrapeCTF = async () => {
+	try {
+		const response = await axios.get('https://ctftime.org/api/v1/events/?limit=100&start=1422019499&finish=1423029499');
+		return response;
+	} catch (error) {
+		throw new Error('An error occurred while scraping the CTF events.');
+	}
+};
+
+export { scrapeLeetcode, scrapeCodechef, scrapeCodeforces, scrapeCTF };
