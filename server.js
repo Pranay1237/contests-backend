621fbd16a0c1f9ec73eab98b5c4fff5929939db6
Pranay1237 <mudhirajpranay30@gmail.com>
2024-04-20 12:25:28 +0530

    small changes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import { scrapeCodechef, scrapeLeetcode, scrapeCodeforces, scrapeCTF } from './scraping.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
