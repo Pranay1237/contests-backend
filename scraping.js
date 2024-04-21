2c6f0900b1d15440ea2bc48de907daf75f85ece2
Pranay1237 <mudhirajpranay30@gmail.com>
2024-04-21 10:01:32 +0530

    changed the web scraping for codechef and removed docker

--- scraping.js
+++ scraping.js
@@ -13,60 +13,19 @@ const scrapeLeetcode = async () => {
 
 const scrapeCodechef = async () => {
 
-	// try {
-	// 	const response = await axios.get('https://www.codechef.com/contests');
-	// 	console.log(response.data);
-	// } catch (error) {
-	// 	throw new Error('An error occurred while scraping the contests.');
-	// }
-
-	const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] }); // Add --no-sandbox flag
-	const page = await browser.newPage();
-
-	await page.setViewport({ width: 1080, height: 1024 });
-
-	await page.goto('https://www.codechef.com/contests', { waitUntil: 'networkidle0', timeout: 60000});
-
-	const htmlContent = await page.content();
-
-	await page.close();
-
-	const dom = new JSDOM(htmlContent);
-	const tbody = dom.window.document.querySelectorAll("tbody");
-	const table = tbody[1];
-	const rows = table.querySelectorAll("tr");
-
-	const contests = [];
-
-	for (let i = 0; i < rows.length; i++) {
-		const cols = rows[i].querySelectorAll("td");
-		const code = cols[0].textContent.trim();
-		const name = cols[1].textContent.trim();
-		const start = cols[2].textContent.trim();
-		const duration = cols[3].textContent.trim();
-		const link = cols[1].querySelector("a").getAttribute("href");
-
-		const contest = {
-			code: code,
-			name: name,
-			start: start,
-			duration: duration,
-			link: link
-		};
-
-		contests.push(contest);
-
+	try {
+		const response = await axios.get('https://www.codechef.com/api/list/contests/all?sort_by=START&sorting_order=asc&offset=0&mode=premium');
+		// console.log(response.data);
+		return response.data.future_contests;
+	} catch (error) {
+		throw new Error('An error occurred while scraping the contests.');
 	}
-
-	const contestsJSON = JSON.stringify(contests);
-	return contestsJSON;
 };
 
 const scrapeCodeforces = async () => {
 
 	try {
 		const response = await axios.get('https://codeforces.com/contests');
-		// console.log(response.data);
 
 		const contests = [];
 
